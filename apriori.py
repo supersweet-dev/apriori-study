71b13cbc8e03c4acd08e7d5e360ab7c7c53d3af2
supersweet-dev <jim@nidhoggr.net>
2021-03-14 20:01:19 -0500

    upd8

--- apriori.py
+++ apriori.py
@@ -2,7 +2,13 @@ from apyori import apriori
 from apyori import load_transactions
 
 transactions = load_transactions(open('house-votes-84.data'), delimiter=",")
+        
 
 results = list(apriori(transactions, min_confidence=0.9, min_support=0.3))
+count = 1
 for record in results:
-    print(record)
+    
+    print("rule #", count, ": ", record.items)
+    
+    
+    count = count + 1
